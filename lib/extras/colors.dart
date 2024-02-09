66a3a0cd09ced95db0a8001d386dfdb63d9e74cd
AmirraTarrek <amirat737@gmail.com>
2024-02-09 02:36:34 +0200

    fix: fix custom colors and icons

---
+++ lib/extras/colors.dart
@@ -0,0 +1,14 @@
+// ignore_for_file: non_constant_identifier_names
+
+import 'package:flutter/material.dart';
+class AppColors {
+  static Color white = const Color(0xffffffff);
+  static Color black = const Color(0xff000000);
+  static Color black9 = const Color(0xff999999);
+  static Color black8 = const Color(0xff888888);
+  static Color gray7 = const Color(0xff777777);
+  static Color grayC8 = const Color(0xffC8C8C8);
+  static Color grayE8 = const Color(0xffE8E8E8);
+  static Color gray5 = const Color(0xff555555);
+  static Color orangeff = const Color(0xffFFC107);
+}
