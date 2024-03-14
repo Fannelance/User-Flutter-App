dc391eadf571b47ea403c1bc8e17b7e16cb36222
Amira Tarek <amirat737@gmail.com>
2024-03-15 01:36:00 +0200

    feat: Add search history feature

--- lib/widgets/search_widget.dart
+++ lib/widgets/search_widget.dart
@@ -1,4 +1,3 @@
-import 'package:fannelance/views/workers_view.dart';
 import 'package:fannelance/widgets/service_search_widget.dart';
 import 'package:flutter/material.dart';
 import 'package:fannelance/models/services_model.dart';
@@ -19,28 +18,85 @@ class SearchWidget extends StatefulWidget {
 class SearchWidgetState extends State<SearchWidget> {
   final focus = FocusNode();
 
+  // History method
   final List<String> emptyList = [''];
-  
-  List<String> _getEmptyList() {
-    return emptyList;
+
+  List<ServicesModel> searchHistoryList = [];
+
+  void _addToSearchHistoryList(ServicesModel e) {
+    setState(() {
+      if (!searchHistoryList.contains(e)) {
+        searchHistoryList.insert(0, e);
+      }
+    });
   }
 
+  void _removeFromSearchHistoryList(ServicesModel e) {
+    setState(() {
+      searchHistoryList.remove(e);
+    });
+  }
+
+  List<SearchFieldListItem<ServicesModel>> _getSearchHistoryList() {
+    if (searchHistoryList.isEmpty) {
+      return emptyList
+          .map((e) => SearchFieldListItem<ServicesModel>(
+                '',
+                child: const EmptySearchWidget(),
+              ))
+          .toList();
+    } else {
+      return searchHistoryList
+          .map((e) => SearchFieldListItem<ServicesModel>(
+                '',
+                item: e,
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: ServiceSearchWidget(
+                        obj: e,
+                        addToSearchHistoryList: _addToSearchHistoryList,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {
+                        _removeFromSearchHistoryList(e);
+                      },
+                      icon: const Icon(
+                        Icons.clear,
+                      ),
+                    ),
+                  ],
+                ),
+              ))
+          .toList();
+    }
+  }
+
+  //search method
   List<ServicesModel> _getServicesList() {
     return widget.servicesList;
   }
 
-  //search method
   List<SearchFieldListItem<ServicesModel>> _onSearchTextChanged(String query) {
     final filter = _getServicesList()
         .where((element) =>
             element.serviceName.toLowerCase().contains(query.toLowerCase()))
         .toList();
-    return filter
-        .map((e) => SearchFieldListItem<ServicesModel>(
-              e.serviceName,
-              child: ServiceSearchWidget(obj: e),
-            ))
-        .toList();
+    if (query.isNotEmpty) {
+      return filter
+          .map((e) => SearchFieldListItem<ServicesModel>(
+                '',
+                item: e,
+                child: ServiceSearchWidget(
+                  obj: e,
+                  addToSearchHistoryList: _addToSearchHistoryList,
+                ),
+              ))
+          .toList();
+    } else {
+      return _getSearchHistoryList();
+    }
   }
 
   @override
@@ -53,19 +109,11 @@ class SearchWidgetState extends State<SearchWidget> {
         padding: const EdgeInsets.symmetric(horizontal: 25),
         child: SearchField(
           //suggestions list
-          suggestions: _getEmptyList()
-              .map((e) => SearchFieldListItem<ServicesModel>(''))
-              .toList(),
-          onSuggestionTap: (SearchFieldListItem<ServicesModel> item) {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const WorkersView()),
-            );
-          },
+          suggestions: _getSearchHistoryList(),
           itemHeight: screenWidth / 6.5,
           maxSuggestionsInViewPort: 4,
           suggestionsDecoration: SuggestionDecoration(
-            border: Border.all(color: const Color(0xff999999), width: 0.5),
+            border: Border.all(color: const Color(0xff999999), width: 0.8),
             borderRadius: const BorderRadius.vertical(
               bottom: Radius.circular(10),
             ),
@@ -107,7 +155,6 @@ class SearchWidgetState extends State<SearchWidget> {
           onTapOutside: (pointer) {
             focus.unfocus();
           },
-          // key: const Key('searchfield'),
         ),
       ),
     );
--- lib/widgets/service_search_widget.dart
+++ lib/widgets/service_search_widget.dart
@@ -1,39 +1,73 @@
 import 'package:fannelance/core/utils.dart';
 import 'package:fannelance/models/services_model.dart';
+import 'package:fannelance/views/workers_view.dart';
 import 'package:flutter/material.dart';
 
 class ServiceSearchWidget extends StatelessWidget {
   final ServicesModel obj;
+  final Function addToSearchHistoryList;
+
   const ServiceSearchWidget({
     super.key,
     required this.obj,
+    required this.addToSearchHistoryList,
   });
 
   @override
   Widget build(BuildContext context) {
     final double screenWidth = MediaQuery.of(context).size.width;
 
-    return Padding(
-      padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 16),
-      child: Row(
-        children: [
-          CardServiceWidget(
-            border: 10,
-            height: screenWidth / 7.5,
-            width: screenWidth / 7.5,
-            image: obj.serviceIcon,
-            padding: screenWidth / 60,
-          ).cardWidget,
-          const SizedBox(
-            width: 16,
-          ),
-          Text(
-            obj.serviceName,
-            style: TextStyle(
-              fontSize: screenWidth / 20,
+    return InkWell(
+      onTap: () {
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => const WorkersView()),
+        );
+        addToSearchHistoryList(obj);
+      },
+      child: Padding(
+        padding: const EdgeInsets.symmetric(vertical: 5, horizontal: 16),
+        child: Row(
+          children: [
+            CardServiceWidget(
+              border: 10,
+              height: screenWidth / 7.5,
+              width: screenWidth / 7.5,
+              image: obj.serviceIcon,
+              padding: screenWidth / 60,
+            ).cardWidget,
+            const SizedBox(
+              width: 15,
             ),
-          ),
-        ],
+            Text(
+              obj.serviceName,
+              style: TextStyle(
+                fontSize: screenWidth / 20,
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+/* Empty Search Widget */
+
+class EmptySearchWidget extends StatelessWidget {
+  const EmptySearchWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
+    return Center(
+      child: Text(
+        'No recent searches',
+        style: TextStyle(
+          fontSize: screenWidth / 20,
+        ),
       ),
     );
   }
