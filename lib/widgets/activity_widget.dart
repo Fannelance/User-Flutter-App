acfe1caf473df4912902e793be11552b71ec09f1
YoussefShaban6 <yosifshaban6@gmail.com>
2024-05-26 21:34:51 +0300

    fix: add high quality image for startup page and fix the activity widget

--- lib/widgets/activity_widget.dart
+++ lib/widgets/activity_widget.dart
@@ -1,65 +1,70 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
-class roactivity extends StatelessWidget {
-  const roactivity({super.key});
+class ActivityWidget extends StatelessWidget {
+  const ActivityWidget({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Row(
-               children: [
-            Padding(
-              padding:  EdgeInsets.all(12.0),
-               child:SvgPicture.asset(
-                 'assets/icons/worker.svg',
-                 width: 50,
-                  height: 50,
-)
-,
-            ),
-            const Padding(
-              padding: EdgeInsets.only(bottom: 15),
-              child: Column(
-                children: [
-                  Column(
-                    children:  [
-                      Text.rich(
-                        TextSpan
-                      (text: "Ahmed Hassan ",style: 
-                      TextStyle(fontFamily: "Gilroy-Bold",fontSize: 18),
-                      children: <TextSpan>[
-                        TextSpan(text: "| Carpenter",style: TextStyle(fontFamily: "Gilroy-Meduim",fontSize: 18))
-                      ]
-                      
-                      ),
-                      
-                      
-                      ),                
-                     Padding(
-                       padding: EdgeInsets.only(right: 150),
-                       child: Row(
-                         children: [
-                           Text("50/hour",style: TextStyle(fontFamily: "Gilroy-Meduim",fontSize: 18),),    
-                         ], 
-                       ),
-                       
-                     ),                    
-                    ],
-                     
+      children: [
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10.0, vertical: 5.0),
+          child: SvgPicture.asset(
+            'assets/icons/worker.svg',
+            width: screenWidth / 7.5,
+          ),
+        ),
+        Expanded(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text.rich(
+                TextSpan(
+                  text: "Ahmed Hassan ",
+                  style: TextStyle(
+                    fontFamily: "Gilroy-Bold",
+                    fontSize: screenWidth / 24,
                   ),
-                  
-                ],
-                
+                  children: <TextSpan>[
+                    TextSpan(
+                      text: "| Carpenter",
+                      style: TextStyle(
+                        fontFamily: "Gilroy-Medium",
+                        fontSize: screenWidth / 24,
+                      ),
+                    ),
+                  ],
+                ),
               ),
-            ),
-            const Padding(
-              padding: EdgeInsets.only(top: 18),
-              child: Text('5th Jan 11:00', style: TextStyle(fontFamily: "Gilroy-Meduim",fontSize: 16),),
-            ),
-            
-          ],
-          
-          
-            );
-}
+              SizedBox(
+                width: screenWidth / 1.3,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      "50/hour",
+                      style: TextStyle(
+                        fontFamily: "Gilroy-Medium",
+                        fontSize: screenWidth / 26,
+                      ),
+                    ),
+                    Text(
+                      '5th Jan 11:00',
+                      style: TextStyle(
+                        fontFamily: "Gilroy-Medium",
+                        fontSize: screenWidth / 26,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ],
+    );
+  }
 }
